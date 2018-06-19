0b619f20d7b70615dbe3d2fb0cd1ea456a165664
wvha <Wvha17@gmail.com>
2018-06-18 18:28:35 -0700

    complete tests and code for queue

---
+++ spec/queueSpecs.js
@@ -0,0 +1,30 @@
+describe('queue', function() {
+
+  beforeEach(function() {
+    queue = new Queue();
+  });
+
+  it('should have size of zero for a new queue', function() {
+    expect(queue.size()).to.equal(0);
+  });
+
+  it('should have a size of 2 after adding two items', function() {
+    queue.upqueue('a');
+    queue.upqueue('b');
+    expect(queue.size()).to.equal(2);
+  });
+
+  it('should have size of 0 after removing more items vs added', function() {
+    queue.upqueue('a');
+    queue.dequeue();
+    queue.dequeue();
+    expect(queue.size()).to.equal(0);
+  });
+
+  it('removes the oldest item in the list', function() {
+    queue.upqueue('a');
+    queue.upqueue('b');
+    expect(queue.dequeue()).to.equal('a');
+  });
+
+})
